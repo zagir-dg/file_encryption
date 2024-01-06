6bdcb32f5ceb3f9df555e686132c02efb88b8970
Zagir <zainudinov.zagir@gmail.com>
2024-01-07 02:33:58 +0300

    walking_by_dirs func has been added

--- main.py
+++ main.py
@@ -20,3 +20,20 @@ def encryption(file, password):
     # removing source file
     os.remove(file)
     
+
+# directories scan function 
+def walking_by_dirs(dir, password):
+    # get over all the subdirectories
+    for name in os.listdir(dir):
+        path = os.path.join(dir, name)
+
+        # if we've found file then encrypt it
+        if os.path.isfile(path):
+            try:
+                encryption(path, password)
+            except Exception as ex:
+                print(ex)
+        # else continue searching files
+        else:
+            walking_by_dirs(path, password)
+            
